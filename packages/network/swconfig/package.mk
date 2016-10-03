f0c143e4e3f184c55d73e4a826a2f34f42d2a90b
Venelin Efremov <veffremov.ve@gmail.com>
2016-10-03 01:46:51 -0700

    Removed odd patched from the kernel. Fix issue in the build script. Added the swconfig tool from openwrt.

--- packages/network/swconfig/package.mk
+++ packages/network/swconfig/package.mk
@@ -16,27 +16,18 @@
 #  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-PKG_NAME="network"
-PKG_VERSION=""
-PKG_REV="1"
+PKG_NAME="swconfig"
+PKG_VERSION="15"
+PKG_REV="05"
 PKG_ARCH="any"
-PKG_LICENSE="various"
-PKG_SITE="http://www.openelec.tv"
-PKG_URL=""
-PKG_DEPENDS_TARGET="toolchain connman iana-etc ethtool openssh"
+PKG_LICENSE="GPLv2"
+PKG_SITE="http://openwrt.org"
+PKG_DEPENDS_TARGET="toolchain libnl"
 PKG_PRIORITY="optional"
-PKG_SECTION="virtual"
-PKG_SHORTDESC="network: Metapackage for packages to install network support"
-PKG_LONGDESC="network: Metapackage for various packages to install network support"
+PKG_SECTION="network"
+PKG_SHORTDESC="swconfig tool"
+PKG_LONGDESC="swconfig tool"
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
-
-if [ "$BLUETOOTH_SUPPORT" = "yes" ]; then
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET bluez"
-fi
-
-if [ "$SWCONFIG_SUPPORT" = "yes" ]; then
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET swconfig"
-fi
 
